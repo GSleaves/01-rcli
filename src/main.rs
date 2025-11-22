02b2aa02ad9a873320af418e038897698e3da5e9
GSleaves <1474752165@qq.com>
2025-11-22 16:23:52 +0800

    feature: add csv cli support

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
-fn main() {
-    println!(
-        "Hello, rust!
-    "
-    );
+// rcli csv -i input.csv -o output.json -- header -d ',
+
+use clap::Parser;
+//use anyhow::Ok;
+
+use rcli::{process_csv, Opts, SubCommand};
+
+fn main() -> anyhow::Result<()> {
+    let opts: Opts = Opts::parse();
+    match opts.cmd {
+        SubCommand::Csv(opts) => process_csv(&opts.input, &opts.output)?,
+    }
+    Ok(())
 }
---
+++ src/process.rs
@@ -0,0 +1,29 @@
+use anyhow::Result;
+use csv::Reader;
+use serde::{Deserialize, Serialize};
+use std::fs;
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+struct Player {
+    name: String,
+    position: String,
+    #[serde(rename = "DOB")]
+    dob: String,
+    #[serde(rename = "Nationality")]
+    nationality: String,
+    #[serde(rename = "Kit Number")]
+    kit: u8,
+}
+
+pub fn process_csv(input: &str, output: &str) -> Result<()> {
+    let mut reader = Reader::from_path(input)?;
+    let mut ret: Vec<Player> = Vec::with_capacity(128);
+    for resulit in reader.deserialize() {
+        let record: Player = resulit?;
+        //println!("{:?}", record);
+        ret.push(record);
+    }
+    let json = serde_json::to_string_pretty(&ret)?;
+    fs::write(output, json)?; // => ()
+    Ok(())
+}
